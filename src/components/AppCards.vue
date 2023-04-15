f4c1b09ca6cc11a952cab67ad2bbe62384bd5cac
Cris-NTL <natolicristian92@gmail.com>
2023-04-15 21:57:54 +0200

    Add: AppFooter

--- src/components/AppCards.vue
+++ src/components/AppCards.vue
@@ -18,32 +18,5 @@ export default {
 
 
 <style scoped lang="scss">
-.card {
-    width: calc((100% / 6) - 30px);
-    margin: 20px 15px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    cursor: pointer;
-
-    & h4 {
-        color: white;
-    }
-
-    & .card-image {
-        width: 100%;
-        padding-top: 100%;
-        position: relative;
-
-        & img {
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-            object-position: top;
-            position: absolute;
-            top: 0;
-        }
-    }
-
-}
+@use "../style/app-cards" as *;
 </style>
--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,12 +1,71 @@
 <script>
 export default {
     name: 'AppFooter',
+    data() {
+        return {
+            comic: ["Character", "Comics", "Movies", "TV", "Games", "Videos", "News"],
+            shop: ["Shod DC", "Shop DC Collectilbles"],
+            dc: ["Terms Of Use", "Privacy Policy", "Ad Choices", "Advertising", "Jobs", "Subscriptions", "Talent Workshop", "CPSC Certificates", "Ratings", "Shop Help", "Contact Us"],
+            sites: ["DC", "MAD Magazine", "DC Kids", "DC Universe", "DC Power Visa"]
+        }
+    },
 }
 </script>
 
 <template>
     <div class="container">
+        <div class="wrap">
+            <div class="left-side">
 
+                <div class="links">
+
+                    <h3>DC COMICS</h3>
+                    <ul v-for="(element, index) in comic">
+                        <li>{{ element }}</li>
+                    </ul>
+
+                    <h3>SHOP</h3>
+                    <ul v-for="(element, index) in shop">
+                        <li>{{ element }}</li>
+                    </ul>
+
+                </div>
+
+                <div class="links">
+
+                    <h3>DC</h3>
+                    <ul v-for="(element, index) in dc">
+                        <li>{{ element }}</li>
+                    </ul>
+
+                </div>
+
+                <div class="links">
+
+                    <h3>SITES</h3>
+                    <ul v-for="(element, index) in sites">
+                        <li>{{ element }}</li>
+                    </ul>
+
+                </div>
+            </div>
+
+            <div class="right-side">
+                <div class="logo">
+                    <img src="../assets/img/dc-logo-bg.png" alt="logo">
+                </div>
+            </div>
+        </div>
+
+        <div class="">
+            <div>
+
+            </div>
+
+            <div>
+
+            </div>
+        </div>
     </div>
 </template>
 
